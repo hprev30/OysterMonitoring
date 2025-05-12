200d1cd03460e4f4a1e4bc8edb596752c654f789
Prevost <Hans.Prevost@FloridaDEP.gov>
2025-05-12 10:40:10 -0400

    saving

--- 22-24Vis.R
+++ 22-24Vis.R
@@ -6,6 +6,7 @@ library(readxl)
 library(ggplot2)
 library(dplyr)
 library(ggtext)
+library(readr)
 
 #####reading in data sets-----
 reef = read_xlsx("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Analysis/2022-2024/Reefs.xlsx")
@@ -15,6 +16,17 @@ oys = read_xlsx("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oy
 clams = read_xlsx("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Analysis/2022-2024/Clams.xlsx")
 percents = read.csv("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Analysis/2022-2024/percent_means.csv")
 
+reefm = read_excel("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Masters/GTM Oyster Monitoring Data Masters (2014-2024).xlsx",  
+                  sheet = "Reefs") 
+
+countsm = read_excel("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Masters/GTM Oyster Monitoring Data Masters (2014-2024).xlsx",  
+                   sheet = "Counts") 
+
+oshellhm = read_excel("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Masters/GTM Oyster Monitoring Data Masters (2014-2024).xlsx",  
+                     sheet = "Oysters") 
+
+mshellhm = read_excel("Z:/Research/OYSTERS/Oyster Monitoring/Oyster Monitoring Data/Oyster Data Masters/GTM Oyster Monitoring Data Masters (2014-2024).xlsx",  
+                      sheet = "Mussels") 
 #####renaming-----
 clams <- within(clams, Region[Region == 'Butler Beach' & Season == 'W23'] <- 'North Butler Beach')
 clams <- within(clams, Region[Region == 'Butler Beach' & Season == 'W24'] <- 'South Butler Beach')
@@ -32,7 +44,8 @@ colnames(counts)[9] <- "Barnacles"
 colnames(counts)[10] <- "Mussels"
 colnames(counts)[11] <- "Clams"
 colnames(reef)[18] <- "Clusters"
-#####ordering, Averages, scaling up coounts to meter squared------
+
+#####ordering, Averages, scaling up counts to meter squared------
 counts$Region = as.factor(counts$Region)
 clams$ShellLength_mm = na.omit(clams$ShellLength_mm)
 clams$ShellLength_mm = as.numeric(clams$ShellLength_mm)
@@ -60,7 +73,7 @@ Rmeans_slope = reef %>% group_by(Region) %>%
 Rmeans_height = reef %>% group_by(Region) %>%
   summarize(mean = mean(ReefHeight_m, na.rm = T))
 Percent_means = reef %>% group_by(Region) %>% summarise(across(everything(), list(mean)))
-write.csv(Percent_means, "percent_means.csv")
+#write.csv(Percent_means, "percent_means.csv")
 Percent_means = Percent_means[,c(1, 21:25)]
 Rmeans_clusters = reef %>% group_by(Region) %>%
   summarize(mean = mean(Clusters, na.rm = T))
@@ -95,16 +108,19 @@ cover_colors = c(
 oys_jit = counts %>% 
   ggplot(aes(x = Region, y = tcounts_o , color = Region)) + geom_violin(trim = F) +
   geom_jitter(alpha = 0.5, width = 0.2, size = 2) +
-  geom_point(data = Rmeans_oys, aes(x = Region, y = mean, size = 4)) +
+  geom_point(data = Rmeans_oys, aes(x = Region, y = mean, size = 4), color = 'black') +
   scale_color_manual(values = sitecolours) +
-  ggrepel::geom_label_repel(data = Rmeans_mus, aes(x = Region, y = mean, 
-                                                  label = round(mean, digits = 2),
-                                                  family = "times new roman"), size = 3.3,
-                            nudge_y = 2.5, nudge_x = 0.2, color = "black") +
+  ggrepel::geom_label_repel(data = Rmeans_oys, aes(x = Region, y = mean, 
+                                                  label = round(mean, digits = 0),
+                                                  family = "times new roman"), 
+                                                  size = 4.5,
+                                                  nudge_y = 2.5, nudge_x = 0.2,
+                                                  color = "black") +
   theme(axis.text = element_text(color = "black"),axis.line = element_line(),
         legend.position = "none", panel.background = element_blank(),
-        plot.title = element_text(hjust = 0.5)) + scale_x_discrete(guide = guide_axis(n.dodge = 2))+
-  labs(x = 'Region', y = bquote(Oyster~Density~(per~m^2))) + 
+        plot.title = element_text(hjust = 0.5), text = element_text(size = 16)) + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
+  labs(x = 'Region', y = bquote(Live~oyster~density~(oysters/m^2))) + 
   theme(panel.spacing = unit(0, 'lines')) +  ggtitle("Oyster Density by Region")
 
 
@@ -115,7 +131,7 @@ Mus_jit = counts %>%
   geom_jitter(alpha = 0.5, width = 0.2, size = 2) +
   geom_point(data = Rmeans_mus, aes(x = Region, y = mean, size = 4)) +
   scale_color_manual(values = sitecolours) +
-  ggrepel::geom_label_repel(data = Rmeans_oys, aes(x = Region, y = mean, 
+  ggrepel::geom_label_repel(data = Rmeans_mus, aes(x = Region, y = mean, 
                                                    label = round(mean, digits = 2),
                                                    family = "times new roman"), size = 3.3,
                             nudge_y = 2.5, nudge_x = 0.2, color = "black") +
@@ -132,15 +148,18 @@ clus_jit = reef %>%
   ggplot(aes(x = Region, y = Clusters, color = Region)) + geom_violin(trim = F) +
   geom_jitter(alpha = 0.5, width = 0.2, size = 2) +
   geom_point(data = Rmeans_clusters, aes(x = Region, y = mean, size = 4)) +
-  geom_point(data = Rmeans_clusters, aes(x= Region, y = mean, size = 4, shape = 1, color = 'black')) +
+  geom_point(data = Rmeans_clusters, aes(x= Region, y = mean, size = 4, 
+                                         shape = 1, color = 'black')) +
   scale_color_manual(values = sitecolours) +
   ggrepel::geom_label_repel(data = Rmeans_clusters, aes(x = Region, y = mean, 
                                                    label = round(mean, digits = 2),
-                                                   family = "times new roman"), size = 3.3,
+                                                   family = "times new roman"),
+                                                  size = 3.3,
                             nudge_y = 2.5, nudge_x = 0.2, color = "black") +
   theme(axis.text = element_text(color = "black"),axis.line = element_line(),
         legend.position = "none", panel.background = element_blank(),
-        plot.title = element_text(hjust = 0.5)) + scale_x_discrete(guide = guide_axis(n.dodge = 2))+
+        plot.title = element_text(hjust = 0.5)) + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
   labs(x = 'Region', y = bquote(Cluster~Counts~(per~0.0625~m^2))) + 
   theme(panel.spacing = unit(0, 'lines')) +  ggtitle("Cluster Density by Region")
 
@@ -192,16 +211,12 @@ Percent_bar = ggplot(percents, aes(fill=CoverType, y=Percent, x=Region)) +
   theme(axis.text = element_text(color = "black"),
         axis.line = element_line(),
         panel.background = element_blank(),
-        plot.title = element_text(hjust = 0.5)) + labs(x = 'Region', y = 'Average Percent Cover', fill = "Cover Type") + 
-  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +  scale_fill_manual(values = cover_colors)
+        plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'Region', y = 'Average Percent Cover', fill = "Cover Type") + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +  
+  scale_fill_manual(values = cover_colors)
 
 Percent_bar
-
-
-
-
-  
-
 
 #####box and whiskers-----
 region_counts_bw = ggplot(counts, aes(x=Region, y = Oysters, fill = Region)) + 
